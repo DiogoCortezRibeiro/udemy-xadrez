ae1434421431fbb835a9147df6e4204afdd04198
Diogo cortez <diogocortez@gmail.com>
2022-02-02 16:24:23 -0300

    Criando classe de tabuleiro;

--- src/main/java/application/TesteApplication.java
+++ src/main/java/application/TesteApplication.java
@@ -1,11 +1,10 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class TesteApplication {
     public static void main(String[] args) {
-        Position pos = new Position(1, 2);
-
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
